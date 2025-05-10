ee3373e9d3cd62cb1b0c273694aa1a8a2cece30a
celcius <juanjjo444@gmail.com>
2025-05-10 00:33:10 -0500

    feat: MVP currency list done!

--- modules/crypto/components/CryptoCurrencyItem.tsx
+++ modules/crypto/components/CryptoCurrencyItem.tsx
@@ -1,8 +1,8 @@
-import { AppThemeColors, useAppTheme } from "@/theme/types";
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { Divider, Icon, Surface, Text } from "react-native-paper";
-import { CryptoCurrency } from "../models";
+import { AppThemeColors, useAppTheme } from '@/theme/types';
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Divider, Icon, Surface, Text } from 'react-native-paper';
+import { CryptoCurrency } from '../models';
 
 interface Props {
   crypto: CryptoCurrency;
@@ -15,16 +15,32 @@ const CryptoCurrencyItem = ({ crypto, onPress }: Props) => {
 
   return (
     <TouchableOpacity onPress={() => onPress(crypto.rank)}>
-      <Surface
-        elevation={0}
-        style={[styles.container, { borderColor: colors.grey600 }]}
-      >
+      <Surface elevation={0} style={[styles.container, { borderColor: colors.grey600 }]}>
         <View style={styles.itemContainer}>
-          <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 10,
+              alignItems: 'center',
+              maxWidth: '70%',
+            }}>
             <Icon color={colors.primary700} size={32} source="currency-usd" />
-            <Text variant="titleSmall" style={{ color: colors.primary700 }}>
-              {crypto.name} ({crypto.symbol})
-            </Text>
+            <View style={{ flexDirection: 'column', gap: 2 }}>
+              <Text
+                variant="titleSmall"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                style={{ color: colors.primary700, flexShrink: 1 }}>
+                {crypto.name}
+              </Text>
+              <Text
+                variant="bodySmall"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                style={{ color: colors.primary700, flexShrink: 1 }}>
+                ({crypto.symbol})
+              </Text>
+            </View>
           </View>
           <View>
             <Text variant="titleSmall" style={{ color: colors.primary700 }}>
@@ -44,9 +60,8 @@ const CryptoCurrencyItem = ({ crypto, onPress }: Props) => {
             <Text style={styles.textInfo}>Cambio 24h</Text>
             <Text
               style={{
-                color: crypto.percentChange24h >= 0 ? "green" : "red",
-              }}
-            >
+                color: crypto.percentChange24h >= 0 ? 'green' : 'red',
+              }}>
               {crypto.percentChange24h.toFixed(2)}%
             </Text>
           </View>
@@ -72,12 +87,12 @@ export const vehicleStyles = (colors: AppThemeColors) =>
       borderRadius: 16,
     },
     itemContainer: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
       marginBottom: 10,
     },
     textInfo: {
-      fontWeight: "700",
+      fontWeight: '700',
     },
   });
